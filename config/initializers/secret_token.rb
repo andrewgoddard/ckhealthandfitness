4daf64e4aa0086ff727d475c327890c4512e2986
Andrew Goddard <andrew.goddard.218a@googlemail.com>
2013-07-06 10:13:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ckhealth::Application.config.secret_token = '55ce4ce87735cb5686d2630420b741ef911bcd5d77acf97353da04e017506e87938de30915fc6a6b245284225ac941e55ccb98175e5caab627c6e695ad7c4c8d'
